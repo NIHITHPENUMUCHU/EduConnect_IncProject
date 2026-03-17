359dde7888a99748b2668bb4e9c2cc478b127666
NIHITHPENUMUCHU <dummy@gmail.com>
2026-03-17 10:01:46 +0000

    day-3

--- server/src/main/java/com/edutech/progressive/dao/CourseDAOImpl.java
+++ server/src/main/java/com/edutech/progressive/dao/CourseDAOImpl.java
@@ -1,5 +1,27 @@
-package com.edutech.progressive.dao;
+@Override
+public int addCourse(Course course) throws SQLException {
+    String sql = "INSERT INTO course (course_name, description, teacher_id) VALUES (?, ?, ?)";
 
-public class CourseDAOImpl {
+    try (Connection con = DatabaseConnectionManager.getConnection();
+         PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
+        ps.setString(1, course.getCourseName());
+        ps.setString(2, course.getDescription());
+
+        if (course.getTeacherId() == 0) {
+            ps.setNull(3, Types.INTEGER);
+        } else {
+            ps.setInt(3, course.getTeacherId());
+        }
+
+        int result = ps.executeUpdate();
+        if (result == 0) return -1;
+
+        ResultSet rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+            return rs.getInt(1);
+        }
+
+        return -1;
+    }
 }
